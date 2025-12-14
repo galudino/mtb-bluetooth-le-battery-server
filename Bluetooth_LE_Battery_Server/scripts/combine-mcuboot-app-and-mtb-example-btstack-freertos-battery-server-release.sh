88ef4fc36f0bbf7370d1fa9d89b984537ee927a0
Gemuele (Gem) Aludino <g.aludino@gmail.com>
2025-12-14 02:55:30 -0500

    Fix combine scripts to use correct app bulid folder

--- Bluetooth_LE_Battery_Server/scripts/combine-mcuboot-app-and-mtb-example-btstack-freertos-battery-server-release.sh
+++ Bluetooth_LE_Battery_Server/scripts/combine-mcuboot-app-and-mtb-example-btstack-freertos-battery-server-release.sh
@@ -23,7 +23,7 @@ function main() {
     local srecord_path_string="/Applications/ModusToolbox/tools_3.5/srecord/bin/srec_cat"
 
     local boot_hex_string="./third-party/mcuboot/boot/cypress/"$mcuboot_app_name_string"/out/PSOC_063_1M/$config/$mcuboot_app_name_string.hex"
-    local app_hex_string="./build/TARGET_CYBLE-416045-EVAL/$config/$app_name_string.hex"
+    local app_hex_string="./build/APP_CYBLE-416045-EVAL/$config/$app_name_string.hex"
     local combined_hex_string="./$combined_name_string_timestamped.hex"
 
     "$srecord_path_string" "$boot_hex_string" -Intel "$app_hex_string" -Intel -o "$combined_hex_string" -Intel
