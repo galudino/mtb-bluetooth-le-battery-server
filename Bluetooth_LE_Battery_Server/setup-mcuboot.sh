3d5141b28696deb7e53d8af999cd6604e8e50460
Gemuele (Gem) Aludino <g.aludino@gmail.com>
2025-12-14 01:40:15 -0500

    Add a script to copy the PSoC 6 flashmap to the mcuboot submodule
    Intended to be invoked shortly after cloning this repo and the mcuboot submodule.

---
+++ Bluetooth_LE_Battery_Server/setup-mcuboot.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+## Copies the PSoC 6 flashmap to the mcuboot/boot/cypress directory
+cp psoc63_1m_cm0_int_swap_single.json third-party/mcuboot/boot/cypress/psoc63_1m_cm0_int_swap_single.json
+
